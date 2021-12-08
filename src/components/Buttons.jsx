ca58a6a663e698dd90d64fc8de7255e6e83a6511
tmz81 <tmz_81@hotmail.com>
2021-12-07 22:04:09 -0300

    CreateButtons 🚀

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,7 @@
+import "../styles/Buttons.css";
+
+export default function Buttons(props){
+  return(
+    <button className="button">{props.label}</button>
+  )
+};
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,9 +1,26 @@
 import "../styles/Calculator.css";
+import Buttons from "./Buttons";
 
 function Calculator(){
   return(
     <div className="main">
-        
+      <Buttons label="AC"/>
+      <Buttons label="/"/>
+      <Buttons label="7"/>
+      <Buttons label="8"/>
+      <Buttons label="9"/>
+      <Buttons label="*"/>
+      <Buttons label="4"/>
+      <Buttons label="5"/>
+      <Buttons label="6"/>
+      <Buttons label="-"/>
+      <Buttons label="1"/>
+      <Buttons label="2"/>
+      <Buttons label="3"/>
+      <Buttons label="+"/>
+      <Buttons label="0"/>
+      <Buttons label="."/>
+      <Buttons label="="/>
     </div>
   )
 };
